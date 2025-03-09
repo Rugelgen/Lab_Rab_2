82e4de2cd32438e865e28f11e9bbd956595ed0bb
Rugelgen <Oleg.zirkunov@gmail.com>
2025-03-10 01:43:38 +0300

    Update 1.py
    исправлены предыдущие ошибки 
    1) Все названия классов написаны с большой буквы
    2)doctest.testmod() запущен внизу кода (а не вначале)
    3) При инициализации объекта класса Wallet добавлена проверка на отрицательное число (дабы сумма в кошельке не могла быть менее 0)

--- ЛР1/1.py
+++ ЛР1/1.py
@@ -1,16 +1,16 @@
 # TODO Написать 3 класса с документацией и аннотацией типов
 from typing import Union
 import doctest
-doctest.testmod()
 
-class fridge:
+
+class Fridge:
     def __init__(self, status: bool, content: str):
         """
         инициализация объекта холодильник
         пареметры холодильник:
         :param status: информация о том, закрыт холодильник или открыт
         :param content: информация о содержании холодильника
-        >>>Xolodilnik_1 = fridge(False,слон)  # инициализация экземпляра класса
+        >>>Xolodilnik_1 = Fridge(False,"слон")  # инициализация экземпляра класса
         """
         self.status = False
         self.content = None
@@ -43,7 +43,7 @@ class fridge:
         else:
             print("холодильник закрыт")
 
-    def put_in_fridge(self, predmet:str):
+    def put_in_fridge(self, predmet: str):
         """"
         Позволяет положить предмет в холодильник, если тот открыт и в нём ничего нет
         """
@@ -65,21 +65,21 @@ class fridge:
         elif self.content == None:
             print("В холодильнике ничего нет")
 
-class wallet:
-    def __init__(self, status: Union[int,float], color: str):
+class Wallet:
+    def __init__(self, status: Union[int,float] = 0, color: str = None):
         """
         инициализация объекта Кошелёк
         пареметры Кошелька:
         :param status: информация о количесвте денег в колешьке
         :param color: информация о цвете колешелька
         пример:
-        >>>Koshelek_1 = wallet(0,чёрный)  # инициализация экземпляра класса
+        >>>Koshelek_1 = Wallet(0,"чёрный")  # инициализация экземпляра класса
         """
-        self.status = 0
-        self.color = None
 
         if not isinstance(status, Union[int,float]):
             raise TypeError("Статус должен быть числовым значением")
+        elif status < 0:
+            raise ValueError("баланс кошелька не может быть отрицательным")
         self.status = status
 
         if not isinstance(color, str):
@@ -109,7 +109,7 @@ class Fruit:
         :param weight: Вес фрукта в граммах
         :param colour: Цвет фрукта
         Примеры:
-        >>> fruit = Fruit(pear, 25.8, yellow)  # инициализация экземпляра класса
+        >>> fruit = Fruit("pear", 25.8, "yellow")  # инициализация экземпляра класса
         """
         if not isinstance(name, str):
             raise TypeError("Название должно быть типа str")
@@ -125,13 +125,13 @@ class Fruit:
             raise TypeError("Цвет должен быть типа str")
         self.color = colour
 
-    def cut(self, piece:int) -> float:
+    def cut(self, piece:int) -> (float, int):
         """
         Порезать фрукт на равные куски и вывести массу каждого из них.
         :param piece: Количество кусков
         :return: Масса куска
         Примеры:
-        >>> fruit = Fruit(banana, 100, yellow)
+        >>> fruit = Fruit("banana", 100, "yellow")
         >>> fruit.cut(5)
         """
         if not isinstance(piece, int):
@@ -148,3 +148,4 @@ class Fruit:
 if __name__ == "__main__":
     # TODO работоспособность экземпляров класса проверить с помощью doctest
     pass
+doctest.testmod()
